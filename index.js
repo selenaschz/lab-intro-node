26e057ace2b056d5645072a509816e55e2dd5d0a
Selena Sánchez <sele71098@gmail.com>
2024-11-09 11:30:10 +0100

    Finished Lab

--- index.js
+++ index.js
@@ -1,17 +1,61 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.length = this.items.length;
+    this.items.sort((a, b) => a - b);
+  }
 
-  get(pos) {}
+  get(pos) {
+    const searchValue = this.items[pos];
+    if ( !searchValue ) {
+      throw new Error ("OutOfBounds");
+    }
+    
+    return searchValue;
+    
+  }
 
-  max() {}
+  max() {
+    if ( this.length === 0 ) {
+      throw new Error("EmptySortedList");
+    }
 
-  min() {}
+    return Math.max(...this.items);
+  }
 
-  sum() {}
+  min() {
+    if ( this.length === 0) {
+      throw new Error("EmptySortedList");
+    }
 
-  avg() {}
+    return Math.min(...this.items);
+  }
+
+  sum() {
+    if ( this.length === 0) {
+      return 0; 
+    }
+
+    const sum = this.items.reduce((acc, num) => acc + num, 0);
+    return sum;
+  }
+
+  avg() {
+    if ( this.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+
+    const length = this.items.length;
+    const sum = this.sum();
+    return sum / length;
+  }
 }
 
 module.exports = SortedList;
+
+
